1686542c7565ee4531b887137136ebd8d4febaf4
Erik Jonsson <fallowshades@gmail.com>
2025-10-02 17:01:24 +0200

    feature support, polyfill, progress enhance design
    https://css-tricks.com/compiling-css-with-vite-and-lightning-css/

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,28 @@
-import tsconfigPaths from "vite-tsconfig-paths"
-import { defineConfig } from "vitest/config"
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
+import browserslist from 'browserslist'
+import { browserslistToTargets } from 'lightningcss'
 export default defineConfig({
-	plugins: [tsconfigPaths()],
-	test: {
-		globals: true,
-		css: true,
-		coverage: {
-			all: false,
-			include: ["app/**"],
-			reporter: ["text", "json-summary", "json"],
-			reportOnFailure: true,
-		},
-	},
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+    css: true,
+    coverage: {
+      all: false,
+      include: ['app/**'],
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    },
+  },
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('>= 0.25%')),
+      include: Features.LightDark | Features.Colors,
+    },
+  },
+  build: {
+    cssMinify: 'lightningcss',
+  },
 })
