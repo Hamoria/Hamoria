41fce6cec44538bb821458848740a7b359c3697b
Erik Jonsson <fallowshades@gmail.com>
2025-10-02 13:26:57 +0200

    sidebar and search

---
+++ app/components/layout/app-sidebar.tsx
@@ -0,0 +1,33 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from '~/components/ui/sidebar'
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar
+      collapsible='icon'
+      variant='floating'
+      {...props}>
+      {/* <SidebarHeader>
+        <TeamSwitcher teams={sidebarData.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        {sidebarData.navGroups.map((props) => (
+          <NavGroup
+            key={props.title}
+            {...props}
+          />
+        ))}
+        
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={sidebarData.user} />
+      </SidebarFooter>
+      <SidebarRail /> */}
+      sidebar
+    </Sidebar>
+  )
+}
--- app/routes/_dashboard._courses.home.tsx
+++ app/routes/_dashboard._courses.home.tsx
@@ -1,7 +1,21 @@
+import { Header } from '~/components/layout/Header'
+import { Main } from '~/components/layout/Main'
+import { Search } from '~/components/Search'
 export function loader() {
   return null
 }
 
 export default function Component() {
-  return <div>Course Home</div>
+  return (
+    <>
+      <Header>
+        {/* <TopNav links={topNav} /> */}
+        <div className='ml-auto flex items-center space-x-4'>
+          <Search />
+        </div>
+      </Header>
+
+      <Main>{/* Main content goes here */}</Main>
+    </>
+  )
 }
